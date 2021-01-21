530a73c5ce82f3ecdf227cac99fcafe6e2f3cfb1
Ali Akhtari <aaliiaakhtarii@gmail.com>
2021-01-21 22:34:04 +0330

    [min] Add install_requires

--- setup.py
+++ setup.py
@@ -18,5 +18,10 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'python-dotenv',
+        'requests',
+        'warrant'
+    ],
     python_requires='>=3.8',
 )
