201f4a2a5e4f0feb848d6bf04f281433814e9ea9
Ali Akhtari <aaliiaakhtarii@gmail.com>
2021-01-22 21:22:00 +0330

    [refactor] Move all variables to env vars

--- agron_load/__init__.py
+++ agron_load/__init__.py
@@ -9,24 +9,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class Auth:
-    def __init__(self):
-        self.USER_POOL_ID = os.environ["USER_POOL_ID"]
-        self.CLIENT_ID = os.environ["CLIENT_ID"]
+def login():
+    USER_POOL_ID = os.environ["USER_POOL_ID"]
+    CLIENT_ID = os.environ["CLIENT_ID"]
+    COGNITO_PASSWORD = os.environ["COGNITO_PASSWORD"]
+    COGNITO_USERNAME = os.environ["COGNITO_USERNAME"]
 
-    def login(self, username, password):
-        client = Cognito(self.USER_POOL_ID, self.CLIENT_ID, username=username)
-        client.authenticate(password)
-        return client
+    client = Cognito(USER_POOL_ID, CLIENT_ID,
+                     username=COGNITO_USERNAME)
+    client.authenticate(COGNITO_PASSWORD)
+    return client
 
 
 class Loader:
-    def __init__(self, ticker, datatype, username, password):
+    def __init__(self, ticker, datatype):
         base_url = os.environ["API_BASE_URL"]
         url = f"{base_url}/{datatype}/{ticker}"
         self.url = url
 
-        cognitoClient = Auth().login(username, password)
+        cognitoClient = login()
         self.idToken = cognitoClient.id_token
 
     def load(self, data):
